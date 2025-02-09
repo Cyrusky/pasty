3b99769f7b3066f239a901261127042f926b0159
Bo.Jin <bo.jin@borgor.cn>
2025-02-09 13:14:28 +0800

    Refactor icons

---
+++ src/components/icons/index.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import { SVGAttributes, FunctionComponent } from 'react';
+export { default as IconMinimize } from './IconMinimize';
+export { default as IconHome } from './IconHome';
+
+interface Props extends Omit<SVGAttributes<SVGElement>, 'color'> {
+  name: 'minimize' | 'home';
+  size?: number;
+  color?: string | string[];
+}
+
+declare const IconFont: FunctionComponent<Props>;
+
+export default IconFont;
