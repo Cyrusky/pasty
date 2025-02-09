2df1400cde295c6f6789c3b06e35518e2b6aa0c7
Bo.Jin <bo.jin@borgor.cn>
2025-02-09 15:02:07 +0800

    feat: import the buttons style on title bar

--- src/libs/components/TitleBar/components/buttons.tsx
+++ src/libs/components/TitleBar/components/buttons.tsx
@@ -1,3 +1,4 @@
+import "./buttons.less";
 import { minimize } from "@/libs/components/TitleBar/utils.ts";
 import { FC } from "react";
 import { GiPin } from "react-icons/gi";
@@ -5,7 +6,7 @@ import { GiPin } from "react-icons/gi";
 export const WindowButtons: FC = () => {
   return (
     <div className="button-container">
-      <div className={"button button-active"} onClick={minimize}>
+      <div className={"button"} onClick={minimize}>
         <GiPin />
       </div>
     </div>
